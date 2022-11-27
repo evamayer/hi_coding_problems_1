ba13f3cfb83833f2f45c3747ef98187cc84670fe
Eva Mayer <eva.mayer@hyperisland.se>
2022-11-27 22:16:04 +0100

    solved problem 4

---
+++ Vangelis_CodingProblems_4.py
@@ -0,0 +1,5 @@
+feet = float(input("Please enter your length measurement in feet: "))
+inches = feet * 12
+yards = feet / 3
+miles = feet / 5280
+print(feet, "feet is", round(inches, 4), "inches,", round(yards, 4), "yards and", round(miles, 4), "miles")
